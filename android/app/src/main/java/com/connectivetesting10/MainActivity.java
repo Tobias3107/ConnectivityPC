4092e49574942fe132be79adc3b0e38fc3cea245
Tobias Karmann <Tobiaskarmann13@hotmail.com>
2021-05-24 00:42:11 +0200

    Change APP name to ConnectivityPC Updated ReadMe

--- android/app/src/main/java/com/connectivetesting10/MainActivity.java
+++ android/app/src/main/java/com/connectivetesting10/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "connectiveTesting10";
+    return "ConnectivityPC";
   }
 }
--- ios/ConnectivityPCTests/ConnectivityPCTests.m
+++ ios/ConnectivityPCTests/ConnectivityPCTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface connectiveTesting10Tests : XCTestCase
+@interface ConnectivityPCTests : XCTestCase
 
 @end
 
-@implementation connectiveTesting10Tests
+@implementation ConnectivityPCTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
